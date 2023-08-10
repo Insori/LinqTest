779d66e13c0f5c14bdcf93748906fe8eb6c7e101
Insori <s2110@e-mirim.hs.kr>
2023-08-10 13:47:35 +0900

    Movie 클래스 생성

---
+++ LinqTest/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    internal class Movie
+    {
+        public string Name { get; set; }
+        public int ReleaseDate { get; set; }
+        public string Country { get; set; }
+    }
+}
