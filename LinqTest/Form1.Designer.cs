29f6ab27d640d6217b30f927ac74706d3b62fefb
Insori <s2110@e-mirim.hs.kr>
2023-08-10 14:20:06 +0900

    Movie 객체 연동

--- LinqTest/Form1.Designer.cs
+++ LinqTest/Form1.Designer.cs
@@ -28,68 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.movieBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.releaseDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movieBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 112);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.releaseDateDataGridViewTextBoxColumn,
+            this.countryDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.movieBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 326);
+            this.dataGridView1.Size = new System.Drawing.Size(343, 426);
             this.dataGridView1.TabIndex = 0;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(639, 12);
+            this.button5.Location = new System.Drawing.Point(577, 292);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(149, 94);
+            this.button5.Size = new System.Drawing.Size(211, 146);
             this.button5.TabIndex = 5;
             this.button5.Text = "한국 영화만 보기";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(485, 12);
+            this.button4.Location = new System.Drawing.Point(361, 292);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 94);
+            this.button4.Size = new System.Drawing.Size(210, 146);
             this.button4.TabIndex = 4;
             this.button4.Text = "2015년 이후에 개봉한 영화만 보기";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(333, 12);
+            this.button3.Location = new System.Drawing.Point(577, 163);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 94);
+            this.button3.Size = new System.Drawing.Size(211, 123);
             this.button3.TabIndex = 3;
             this.button3.Text = "개봉년도 순으로 보기";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(178, 12);
+            this.button2.Location = new System.Drawing.Point(361, 163);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 94);
+            this.button2.Size = new System.Drawing.Size(210, 123);
             this.button2.TabIndex = 2;
             this.button2.Text = "제목 이름 순으로 보기";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(361, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 94);
+            this.button1.Size = new System.Drawing.Size(427, 145);
             this.button1.TabIndex = 1;
             this.button1.Text = "영화 목록 전체 보기";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // movieBindingSource
+            // 
+            this.movieBindingSource.DataSource = typeof(LinqTest.Movie);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // releaseDateDataGridViewTextBoxColumn
+            // 
+            this.releaseDateDataGridViewTextBoxColumn.DataPropertyName = "ReleaseDate";
+            this.releaseDateDataGridViewTextBoxColumn.HeaderText = "ReleaseDate";
+            this.releaseDateDataGridViewTextBoxColumn.Name = "releaseDateDataGridViewTextBoxColumn";
+            // 
+            // countryDataGridViewTextBoxColumn
+            // 
+            this.countryDataGridViewTextBoxColumn.DataPropertyName = "Country";
+            this.countryDataGridViewTextBoxColumn.HeaderText = "Country";
+            this.countryDataGridViewTextBoxColumn.Name = "countryDataGridViewTextBoxColumn";
             // 
             // Form1
             // 
@@ -105,6 +139,7 @@
             this.Name = "Form1";
             this.Text = "3108 인소리";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movieBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -117,6 +152,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn releaseDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource movieBindingSource;
     }
 }
 
--- LinqTest/Form1.cs
+++ LinqTest/Form1.cs
@@ -12,6 +12,30 @@ namespace LinqTest
 {
     public partial class Form1 : Form
     {
+        static private List<Movie> movies = new List<Movie>()
+        {
+            new Movie() { Name = "엘리멘탈", ReleaseDate = 2023, Country = "미국"},
+            new Movie() { Name = "붏한당: 나쁜 놈들의 세상", ReleaseDate = 2017, Country = "한국"},
+            new Movie() { Name = "어바웃타임", ReleaseDate = 2013, Country = "영국"},
+            new Movie() { Name = "레옹", ReleaseDate = 1995, Country = "프랑스"},
+            new Movie() { Name = "베놈", ReleaseDate = 2018, Country = "미국"},
+            new Movie() { Name = "헌트", ReleaseDate = 2022, Country = "한국"},
+            new Movie() { Name = "히말라야", ReleaseDate = 2015, Country = "한국"},
+            new Movie() { Name = "어메이징 스파이더맨", ReleaseDate = 2015, Country = "미국"},
+            new Movie() { Name = "타이타닉", ReleaseDate = 1997, Country = "미국"},
+            new Movie() { Name = "이터널 선샤인", ReleaseDate = 2004, Country = "프랑스"},
+            new Movie() { Name = "매드 맥스: 분노의 도로", ReleaseDate = 2015, Country = "호주"},
+            new Movie() { Name = "늑대아이", ReleaseDate = 2012, Country = "일본"},
+            new Movie() { Name = "위대한 개츠비", ReleaseDate = 2013, Country = "호주"},
+            new Movie() { Name = "오직 사랑하는 이들만이 살아남는다", ReleaseDate = 2014, Country = "러시아"},
+            new Movie() { Name = "레지던트 이블: 파멸의 날", ReleaseDate = 2017, Country = "러시아"},
+            new Movie() { Name = "나는 네가 지난 여름에 한 일을 알고 있다", ReleaseDate = 1998, Country = "미국"},
+            new Movie() { Name = "겨울왕국", ReleaseDate = 2014, Country = "미국"},
+            new Movie() { Name = "스즈메의 문단속", ReleaseDate = 2022, Country = "일본"},
+            new Movie() { Name = "극한직업", ReleaseDate = 2019, Country = "한국"},
+            new Movie() { Name = "올드보이", ReleaseDate = 2003, Country = "한국"},
+        };
+
         public Form1()
         {
             InitializeComponent();
